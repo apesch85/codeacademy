9a1623b8b83ade39d97a57acedd1621f634a7cb8
gorrilla86 <gorrilla86@gmail.com>
2017-12-06 20:57:10 -0700

    Minesweeper part 1 (I was supposed to manually print each row, but decided to use loops instead.)

--- code_academy/minesweeper/src/minesweeper.js
+++ code_academy/minesweeper/src/minesweeper.js
@@ -1,3 +1,18 @@
-log.console('test')
+const blankLine = '  |   |  '
+const guessLine = '1 |   |  '
+const bombLine = '  | B |  '
+const bombBoard = [guessLine, bombLine, blankLine]
 
-let myvar = 'test'
+
+console.log('This is what an empty board would look like:')
+
+let count = 0;
+while (count < 3) {
+  count += 1;
+  console.log(blankLine);
+}
+
+console.log('This is what a board with a guess and a bomb on it would look like:')
+for (var i = 0; i < 3; i++) {
+    console.log(bombBoard[i]);
+}
