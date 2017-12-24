c3b3add00c488f50c38af4e9b0897f6d89762c0b
gorrilla86 <gorrilla86@gmail.com>
2017-12-24 10:01:21 -0700

    Final version of Minesweeper

--- code_academy/minesweeper/src/board.js
+++ code_academy/minesweeper/src/board.js
@@ -1,24 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs)
-  }
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex)
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game over man, game over! Final Board: ')
-      this._board.print()
-    }
-    else if (!this._board.hasSafeTiles) {
-      console.log('You won (this time)!')
-    }
-    else {
-      console.log('Current Board: ')
-      this._board.print()
-    }
-  }
-}
-
-class Board {
+export class Board {
    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
      this._numberOfBombs = numberOfBombs;
      this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -107,6 +87,3 @@ class Board {
         return board;
       };
     }
-
-const g = new Game(6, 6, 3)
-g.playMove(0, 0)
